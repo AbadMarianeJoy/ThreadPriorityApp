5b1710c1134ff1237d587364f2d07cacdf05532f
YANYAN <YANYAN@YANYAN>
2024-11-20 20:08:57 +0800

    Add project files.

---
+++ ThreadPriorityApp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ThreadPriorityApp
+{
+
+    partial class frmTrackThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(68, 105);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(145, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Run";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(54, 65);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(159, 25);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "-Thread Start-";
+            // 
+            // frmTrackThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 219);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmTrackThread";
+            this.Text = "frmTrackThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+      
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private EventHandler frmTrackThread_Load;
+        private EventHandler frmTrackThread_Load_1;
+        private System.Windows.Forms.Label lblMessage;
+        private EventHandler lblMessages;
+    }
+}
+
---
+++ ThreadPriorityApp/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadPriorityApp
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        public static void Thread1()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process "+ " = " + i);
+                Thread.Sleep(500); 
+            }
+        }
+        public static void Thread2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name +  " Process "+ " = " + i);
+                Thread.Sleep(1500); 
+            }
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+           
+            UpdateLabel("Threads Running...");
+
+            
+            await Task.Run(() =>
+            {
+                Thread threadA = new Thread(Thread1)
+                {
+                    Name = "Thread A",
+                    Priority = ThreadPriority.Highest
+                };
+                Thread threadB = new Thread(Thread2)
+                {
+                    Name = "Thread B",
+                    Priority = ThreadPriority.Normal
+                };
+                Thread threadC = new Thread(Thread1)
+                {
+                    Name = "Thread C",
+                    Priority = ThreadPriority.AboveNormal
+                };
+                Thread threadD = new Thread(Thread2)
+                {
+                    Name = "Thread D",
+                    Priority = ThreadPriority.BelowNormal
+                };
+
+                
+                Console.WriteLine(" -Thread Starts- ");
+                threadA.Start();
+                threadB.Start();
+                threadC.Start();
+                threadD.Start();
+
+               
+                threadA.Join();
+                threadB.Join();
+                threadC.Join();
+                threadD.Join();
+                Console.WriteLine(" -End of Thread- ");
+            });
+
+            
+            UpdateLabel("-Thread End-");
+        }
+
+       
+        private void UpdateLabel(string message)
+        {
+            
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    Label lblMessage = Controls["lblMessage"] as Label;
+                    if (lblMessage != null)
+                    {
+                        lblMessage.Text = message;
+                    }
+                }));
+            }
+            else
+            {
+                Label lblMessage = Controls["lblMessage"] as Label;
+                if (lblMessage != null)
+                {
+                    lblMessage.Text = message;
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
+
+       
